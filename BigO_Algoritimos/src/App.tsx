1f9a099fe6dadaa6aaa2211a9089ee4f02aa6f27
Apenas1Hugo <hugorodriguesm27@gmail.com>
2026-07-14 22:39:04 -0300

    Start project

---
+++ BigO_Algoritimos/src/App.tsx
@@ -0,0 +1,13 @@
+
+import "./App.css";
+
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-950 px-6 py-16 text-slate-50">
+      <h3>inicio do projeto</h3>
+    </div>
+  );
+}
+
+export default App;
