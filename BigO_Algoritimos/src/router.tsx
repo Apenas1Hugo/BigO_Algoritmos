05241410160808843a9b4de585776cd78b063ac5
Apenas1Hugo <hugorodriguesm27@gmail.com>
2026-07-17 11:26:11 -0300

    RootLayout
    RootLayout criado para sempre ser exibido a navbar e o footer.

--- BigO_Algoritimos/src/router.tsx
+++ BigO_Algoritimos/src/router.tsx
@@ -1,40 +1,44 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import { Navbar } from "./components/Layout/Navbar"
-import { Footer } from "./components/Layout/Footer";
+import { RootLayout } from "./components/Layout/RootLayout";
+
 
 export const router = createBrowserRouter([
   {
-    path: "/",
-    element: (
-      <>
-      <Navbar></Navbar>
-      <Footer></Footer>
-      </>
-    ),
-  },
-  {
-    path: "/dividir-e-conquistar",
-    element: (
-      <div className="min-h-screen bg-slate-950 px-6 py-16 text-slate-50">
-        <h3>Dividir e conquistar</h3>
-      </div>
-    ),
-  },
-  {
-    path: "/algoritmos-quadraticos",
-    element: (
-      <div className="min-h-screen bg-slate-950 px-6 py-16 text-slate-50">
-        <h3>Algoritmos Quadráticos</h3>
-      </div>
-    ),
-  },
-  {
-    path: "/arvore",
-    element: (
-      <div className="min-h-screen bg-slate-950 px-6 py-16 text-slate-50">
-        <h3>Árvore</h3>
-      </div>
-    ),
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <div className="min-h-screen bg-slate-950 px-6 py-16 text-slate-50">
+            <h3>Inicio</h3>
+          </div>
+        ),
+      },
+      {
+        path: "/dividir-e-conquistar",
+        element: (
+          <div className="min-h-screen bg-slate-950 px-6 py-16 text-slate-50">
+            <h3>Dividir e conquistar</h3>
+          </div>
+        ),
+      },
+      {
+        path: "/algoritmos-quadraticos",
+        element: (
+          <div className="min-h-screen bg-slate-950 px-6 py-16 text-slate-50">
+            <h3>Algoritmos Quadráticos</h3>
+          </div>
+        ),
+      },
+      {
+        path: "/arvore",
+        element: (
+          <div className="min-h-screen bg-slate-950 px-6 py-16 text-slate-50">
+            <h3>Árvore</h3>
+          </div>
+        ),
+      },
+    ],
   },
 ]);
